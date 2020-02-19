58c8ba74e3919d6ec754cbf140a3c335e9becce8
coin-lobby <admin@coin-lobby.com>
2020-02-19 07:44:07 -0700

    adj

--- src/qt/coinlobby.cpp
+++ src/qt/coinlobby.cpp
@@ -581,8 +581,8 @@ int main(int argc, char *argv[])
     QTextCodec::setCodecForCStrings(QTextCodec::codecForTr());
 #endif
 
-    Q_INIT_RESOURCE(coinlobby);
-    Q_INIT_RESOURCE(coinlobby_locale);
+//    Q_INIT_RESOURCE(coinlobby);
+//    Q_INIT_RESOURCE(coinlobby_locale);
 
     BitcoinApplication app(argc, argv);
 #if QT_VERSION > 0x050100
